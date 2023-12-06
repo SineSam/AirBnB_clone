e0ed33c96ac28d23a49247ace35e36750f9851e2
Sine <ssnngcongo@gmail.com>
2023-12-06 12:19:42 -0800

    updated base model to handle key word arguments

--- models/base_model.py
+++ models/base_model.py
@@ -5,16 +5,18 @@ from datetime import datetime
 import uuid
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = uuid.uuid4()
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        self.kwargs = kwargs
 
     def save(self):
-        self.updated_at
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         my_dict = dict(id='str(self.id',
                 created_at='self.created_at.isoformat()',
                 updated_at='self.updated_at.isoformat')
+        my_dict.update(self.kwargs)
         return my_dict
